9965d2122b84796df92962bf5bb444bd5b808ab9
sr1208 <158018398+sr1208@users.noreply.github.com>
2024-02-12 15:15:14 +0700

    Add files via upload

---
+++ styles/amphoe_1_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_amphoe_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "18.2px \'TH Sarabun New\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("AMP_TH") !== null) {
+        labelText = String(feature.get("AMP_TH"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.748}),fill: new ol.style.Fill({color: 'rgba(27,195,229,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
